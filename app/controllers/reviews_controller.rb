336af3170e193a616f4c7ed75497830171673dbe
Nick Roma <nikoandpiko@gmail.com>
2021-04-06 14:34:18 +0900

    Fixes styling, syntax in app

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,19 @@
 class ReviewsController < ApplicationController
-
-    def create
-        puts params
-        review = Review.new(review_params)
-        toilet = Toilet.find(params[:toilet])
-        review.toilet = toilet
-        if review.save!
-            render json: {toilet: toilet}.to_json(include: [:reviews, :photo_urls])
-        else
-            render json: {errors: review.errors}
-        end
+  def create
+    puts params
+    review = Review.new(review_params)
+    toilet = Toilet.find(params[:toilet])
+    review.toilet = toilet
+    if review.save!
+      render json: { toilet: toilet }.to_json(include: %i[reviews photo_urls])
+    else
+      render json: { errors: review.errors }
     end
-    private
+  end
 
-    def review_params
-        params.require(:review).permit(:cleanliness_score, :content, :toilet_id)
-    end
+  private
 
-
+  def review_params
+    params.require(:review).permit(:cleanliness_score, :content, :toilet_id)
+  end
 end
--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,14 +1,14 @@
 class ToiletsController < ApplicationController
-    def toilets_near_me
-      toilets = Toilet.where("latitude > ? - 0.015 AND latitude < ? + 0.015 AND longitude > ? - 0.015 AND longitude < ? + 0.015",
-        params[:latitude], params[:latitude], params[:longitude], params[:longitude])
-      p toilets
-      render json: { toilets: toilets }.to_json(include: [:reviews, :photo_urls])
+  def toilets_near_me
+    toilets = Toilet.where('latitude > ? - 0.015 AND latitude < ? + 0.015 AND longitude > ? - 0.015 AND longitude < ? + 0.015',
+                           params[:latitude], params[:latitude], params[:longitude], params[:longitude])
+    p toilets
+    render json: { toilets: toilets }.to_json(include: %i[reviews photo_urls])
+  end
 
-    end
+  private
 
-    private
-    def toilets_params
-      params.require(:toilets).permit(:latitude, :longitude)
-    end
+  def toilets_params
+    params.require(:toilets).permit(:latitude, :longitude)
+  end
 end
